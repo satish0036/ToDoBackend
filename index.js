9137f6d1ce8dd8bf5820609d979b183219885396
satish0036 <satish0036kumar@gmail.com>
2024-03-21 10:38:17 +0530

    cors and other update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// server.js
+import dotenv from 'dotenv';
+import cors from "cors"
 import  express  from 'express';
 import bodyParser from 'body-parser';
 import AuthRoutes from './routes/AuthRoutes.js';
 import PostRoutes from './routes/PostRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 8800;
+dotenv.config();
 
+app.use(cors({
+  origin: ['http://localhost:3000','/', ],
+  credentials: true,
+}));
 
 app.use(bodyParser.json());
 
